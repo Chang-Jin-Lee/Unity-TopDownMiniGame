69c47652380bafe328174e2c00be8b2de0e5b359
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-05-03 23:44:04 +0900

    [chore] Update UI Assets

--- SpriteProject/Assets/Editor/PlayerController/PlayerControllerEditor.cs
+++ SpriteProject/Assets/Editor/PlayerController/PlayerControllerEditor.cs
@@ -8,7 +8,8 @@ public class PlayerControllerEditor : Editor
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-
+        
+        
         SerializedProperty playerModelProp = serializedObject.FindProperty("PlayerModel");
         SerializedProperty playerProp = serializedObject.FindProperty("Player");
 
@@ -23,6 +24,20 @@ public class PlayerControllerEditor : Editor
             if (i < playerModelProp.arraySize)
             {
                 SerializedProperty element = playerModelProp.GetArrayElementAtIndex(i);
+                EditorGUILayout.PropertyField(element, new GUIContent(name));
+            }
+        }
+        
+        SerializedProperty playerAbilityTemplateProp = serializedObject.FindProperty("playerAbilityTemplate");
+        
+        EditorGUILayout.LabelField("Player Ability", EditorStyles.boldLabel);
+        for (int i = 0; i < (int)eCharacterState.Max; i++)
+        {
+            string name = ((eCharacterState)i).ToString();
+
+            if (i < playerAbilityTemplateProp.arraySize)
+            {
+                SerializedProperty element = playerAbilityTemplateProp.GetArrayElementAtIndex(i);
                 EditorGUILayout.PropertyField(element, new GUIContent(name));
             }
         }
